3f7d86e961d90bc7351e6e26e360dc210ecf92a1
Fujimoto <genfujimoto@gmail.com>
2021-12-06 03:03:19 +0900

    add git ignore

--- .build/cotire/main_CXX_prefix.cxx
+++
@@ -1,3 +0,0 @@
-/* cotire.cmake 1.8.1 generated file */
-/* /Users/fujimotogen/home/Labo/PlayGround/C++/Library/PCL/csv_parser/.build/cotire/main_CXX_prefix.cxx */
-#pragma clang system_header
--- .build/cotire/main_CXX_unity.cxx
+++
@@ -1,6 +0,0 @@
-/* cotire.cmake 1.8.1 generated file */
-/* /Users/fujimotogen/home/Labo/PlayGround/C++/Library/PCL/csv_parser/.build/cotire/main_CXX_unity.cxx */
-#ifdef __cplusplus
-#include "/Users/fujimotogen/home/Labo/PlayGround/C++/Library/PCL/csv_parser/src/circditector.cpp"
-#include "/Users/fujimotogen/home/Labo/PlayGround/C++/Library/PCL/csv_parser/src/main.cpp"
-#endif
